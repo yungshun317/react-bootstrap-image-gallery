8e7cc4c20b2465736c1a79ad4eda141efd07a6a5
yungshun317 <seraph317@gmail.com>
2021-07-31 14:48:50 +0800

    [FEAT] Create the React entrypoint

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+
+const rootElement = document.getElementById("root");
+
+render(
+    <React.StrictMode>
+    </React.StrictMode>,
+    rootElement
+);
