09fa0d516aac98d445bf83a1f19a28fef48b576f
yungshun317 <seraph317@gmail.com>
2021-08-01 15:25:14 +0800

    [FEAT] Use Bootstrap's responsive grid system

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import { Container, Row, Col } from "react-bootstrap";
 export default function App() {
     return (
         <>
+            <Container>
+                <Row>
+                    <Col>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
