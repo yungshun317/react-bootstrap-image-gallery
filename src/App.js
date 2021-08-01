1bdd8aabb2fea7596e5ce5bc67f2ab7474439796
yungshun317 <seraph317@gmail.com>
2021-08-01 15:30:37 +0800

    [FEAT] Import and use the `ImageGallery` component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import ImageGallery from "react-image-gallery";
 
 export default function App() {
+    const images = [
+        {
+            original: "https://picsum.photos/1000/600",
+            thumbnail: "https://picsum.photos/250/150"
+        }
+    ];
+
     return (
         <>
             <Container>
                 <Row>
                     <Col>
+                        <div>
+                            <ImageGallery items={images} />
+                        </div>
                     </Col>
                 </Row>
             </Container>
