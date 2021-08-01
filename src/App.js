a221576ec7f10864ad7999bafe98cd46e711d984
yungshun317 <seraph317@gmail.com>
2021-08-01 15:56:08 +0800

    [FEAT] Set `autoPlay` and `showBullets` to `true`

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import ImageGallery from "react-image-gallery";
 export default function App() {
     const images = [
         {
-            original: "https://picsum.photos/1000/600",
-            thumbnail: "https://picsum.photos/250/150"
+            original: "https://picsum.photos/id/317/1000/600",
+            thumbnail: "https://picsum.photos/id/317/250/150"
+        },
+        {
+            original: "https://picsum.photos/id/900/1000/600",
+            thumbnail: "https://picsum.photos/id/900/250/150"
         }
     ];
 
@@ -17,7 +21,7 @@ export default function App() {
                 <Row>
                     <Col>
                         <div>
-                            <ImageGallery items={images} />
+                            <ImageGallery items={images} autoPlay={true} showBullets={true} />
                         </div>
                     </Col>
                 </Row>
